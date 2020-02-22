115cb9eb90a384b3da0e9c2cabaefb70c98e2ba7
Chhekur <820121223505e@gmail.com>
2020-02-23 01:07:36 +0530

    update: SMS API

--- build/lib/react_native/api/sms.js
+++ build/lib/react_native/api/sms.js
@@ -3,7 +3,7 @@ var NativeModules = require('react-native').NativeModules;
 var sms = /** @class */ (function () {
     function sms() {
     }
-    sms.prototype.sendSMS = function (number, message) {
+    sms.prototype.send = function (number, message) {
         return JSON.parse(NativeModules.SMS.sendSMS(number, message));
     };
     return sms;
